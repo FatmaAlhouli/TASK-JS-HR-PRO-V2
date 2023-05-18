d599884e584fc4351cee485403cc8877690154b4
unknown <f_alhouli_1985@hotmail.com>
2023-05-18 17:36:59 +0300

    done

--- index.js
+++ index.js
@@ -2,3 +2,55 @@ const employeesJSON = require('./employees.json');
 const managersJSON = require('./managers.json');
 
 // Code Here
+class employees {
+    constructor() 
+   information = [];
+       name; 
+       positon; 
+       yearJoined;
+       salary;
+      };
+     return  idBadge (employees.positon, employees.name);
+
+    class managers extends employees {
+        constructor( ) {
+            information = [];
+            name; 
+            positon; 
+            yearJoined;
+            salary;
+            bonusPercentage;
+            super(employees);
+            this.magers = `bonusPercentage`;
+    }
+    
+}
+console.log ((this.mager = "bonusPercentage"));
+    const salaryIncrease = ++ increaseAmount;
+
+    const employees = [employees];
+    information.map(employees);
+    
+    const mager = [this.mager];
+    information.map(mager);
+
+   // logManagerInfo () {
+        name ; 
+        since;
+        bouns;
+
+   // }
+
+   // getWorkingYears (getWorkingYears) {
+        const totalNumberOfYear = 2023- year;
+        return (totalNumberOfYear);
+
+  //  }
+
+   const employee1 = "`employeesJSON`";
+   if (this.information.name === "employeesJSON") {
+information.map (`employeesJSON`);
+   }
+
+   const superHeroes = "";
+   
